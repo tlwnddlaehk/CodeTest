6cca85ee405aeb24a555050a9bb3a3831bdb8fca
sijungKim <tlwnddlaehk@naver.com>
2020-05-11 23:00:14 +0900

    Initial commit

---
+++ src/Programers.java
@@ -0,0 +1,33 @@
+import sun.jvm.hotspot.debugger.win32.coff.Characteristics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Programers {
+    public static void main(String[] args){
+        boolean flag;
+        int cnt = 0, num=100;
+        List<Integer> resultList = new ArrayList<>();
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+        for (int i=0; i<=num; i++) {
+            hashMap.
+        }
+
+//        for(int i=2; i<=num; i++){
+//            flag = true;
+//            for(int j=2; j<i; j++){
+//                if(i%j ==0){
+//                    flag = false;
+//                    break;	//마법의 코드
+//                }
+//            }
+//            if(flag == true){
+//                cnt++;
+//                System.out.println(i);
+//            }
+//        }
+
+    }
+}
---
+++ src/ShortestWord.java
@@ -0,0 +1,5 @@
+public class ShortestWord {
+    public static void main(String... args) {
+
+    }
+}
